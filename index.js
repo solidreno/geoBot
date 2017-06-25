aa4866781f055a81bcc563889757b28e4604bb00
a331969 <renaud.bocage@socgen.com>
2017-06-25 12:45:06 +0200

    Get size

--- index.js
+++ index.js
@@ -11,25 +11,33 @@ app.get('/', (req, res) => {
   res.status(200).send('Hello World !');
 });
 
-app.post('/country', jsonParser, (req, res) => {
+app.post('/', jsonParser, (req, res) => {
+  let queryAction = req.body.result.action;
   let isoAlpha2 = req.body.result.parameters.isoAlpha2;
   geoDB.getCountryInfo(isoAlpha2).
   then(function (response) {
+
     let apiAIResponse = {
-      speech: `La capitale de ${response.geonames[0].countryName} est ${response.geonames[0].capital}`,
-      displayText: `La capitale de ${response.geonames[0].countryName} est ${response.geonames[0].capital}`,
+      speech: `Je connais ${response.geonames[0].countryName} mais je n'ai pas compris ce que tu voulais...`,
+      displayText: `Je connais ${response.geonames[0].countryName} mais je n'ai pas compris ce que tu voulais...`,
       source: `api.geonames.org`
     }
+
+    if (queryAction == 'getCapital') {
+      apiAIResponse.speech = `La capitale de ${response.geonames[0].countryName} est ${response.geonames[0].capital}`;
+      apiAIResponse.displayText = apiAIResponse.speech;
+    }
+
+    if (queryAction == 'getSize') {
+      apiAIResponse.speech = `${response.geonames[0].countryName} fait ${response.geonames[0].areaInSqKm} km²`;
+      apiAIResponse.displayText = apiAIResponse.speech;
+    }
+
     res.status(200).json(apiAIResponse);
   })
   .catch(function (error) {
     res.status(500).json(error);
   });
-});
-
-app.post('/', jsonParser, (req, res) => {
-  let body = req.body;
-  res.status(200).send(JSON.stringify(body, null, 2));
 });
 
 app.listen(app.get('port'), () => {
